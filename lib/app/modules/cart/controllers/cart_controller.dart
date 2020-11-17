1ec715b2f486888279cbfec61784e682d8c5d2a7
Marcelo Fernandes <marcelofv12@hotmail.com>
2020-11-17 16:12:32 -0300

    Update cart_controller.dart

--- lib/app/modules/cart/controllers/cart_controller.dart
+++ lib/app/modules/cart/controllers/cart_controller.dart
@@ -49,6 +49,7 @@ class CartController extends GetxController {
 
   String getUserCurrentAdress() {
     return '242nd St Tonganoxie, Kansas (KS), 66086';
+    // teste
   }
 
   void searchOrderIndex(Order order) {
