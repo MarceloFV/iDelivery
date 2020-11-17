6e715c0856ee007184ca56fa335781dac1b0e094
Marcelo Fernandes <marcelofv12@hotmail.com>
2020-11-17 20:51:37 -0300

    Formatar os valores da pagina de produto para formato R$0,00

--- lib/app/modules/product/controllers/product_controller.dart
+++ lib/app/modules/product/controllers/product_controller.dart
@@ -1,6 +1,7 @@
 import 'package:delivery_app/app/data/models/product.dart';
 import 'package:delivery_app/app/modules/cart/controllers/cart_controller.dart';
 import 'package:delivery_app/app/routes/app_pages.dart';
+import 'package:flutter_masked_text/flutter_masked_text.dart';
 import 'package:get/get.dart';
 
 class ProductController extends GetxController {
@@ -14,6 +15,12 @@ class ProductController extends GetxController {
   void onInit() {
     product = Get.arguments['product'];
     super.onInit();
+  }
+
+  void onAmountAddPressed() => _amount.value++;
+
+  void onAmountRemovePressed() {
+    if (_amount.value > 1) _amount.value--;
   }
 
   void onAddPressed() {
@@ -33,9 +40,10 @@ class ProductController extends GetxController {
     Get.back();
   }
 
-  void onAmountAddPressed() => _amount.value++;
-
-  void onAmountRemovePressed() {
-    if (_amount.value > 0) _amount.value--;
+  String fetchMaskedProductValue() {
+    final _moneyTextController =
+        new MoneyMaskedTextController(leftSymbol: 'R\$ ');
+    _moneyTextController.updateValue(product.value);
+    return _moneyTextController.text;
   }
 }
--- lib/app/modules/product/views/product_view.dart
+++ lib/app/modules/product/views/product_view.dart
@@ -12,77 +12,89 @@ class ProductView extends GetView<ProductController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: Container(
-        height: 60,
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 8.0),
-          child: Row(
-            children: [
-              Expanded(
-                child: Obx(() => AmountSelector(
-                      onAdd: controller.onAmountAddPressed,
-                      onRemove: controller.onAmountRemovePressed,
-                      amount: controller.amount,
-                    )),
-              ),
-              Expanded(
-                child: CustomBlueButton(
-                  function: controller.onAddPressed,
-                  text: "Adicionar",
-                ),
-              )
-            ],
-          ),
-        ),
+      appBar: _appBar(),
+      body: _body(),
+      bottomNavigationBar: _bottomNavBar(),
+    );
+  }
+
+  _appBar() {
+    return AppBar(
+      elevation: 0,
+      backgroundColor: Colors.transparent,
+      title: Text(
+        controller.product.storeName.toUpperCase(),
+        style: TextStyle(color: Colors.black, fontSize: 16),
       ),
-      appBar: AppBar(
-        elevation: 0,
-        backgroundColor: Colors.transparent,
-        title: Text(
-          controller.product.storeName.toUpperCase(),
-          style: TextStyle(color: Colors.black, fontSize: 16),
-        ),
-        leading: IconButton(
-          onPressed: () => controller.onBackPressed(),
-          icon: Icon(Icons.arrow_back_ios),
-          color: Colors.black,
-          iconSize: 12,
-        ),
-        actions: [
-          IconButton(
-            icon: Icon(
-              Icons.shopping_cart_outlined,
-              color: Colors.black,
-            ),
-            onPressed: () => controller.onCartPressed(),
+      leading: IconButton(
+        onPressed: () => controller.onBackPressed(),
+        icon: Icon(Icons.arrow_back_ios),
+        color: Colors.black,
+        iconSize: 12,
+      ),
+      actions: [
+        IconButton(
+          icon: Icon(
+            Icons.shopping_cart_outlined,
+            color: Colors.black,
           ),
-          // TODO: Sistema de favoritos
-          // IconButton(
-          //   icon: Icon(
-          //     Icons.favorite_border,
-          //     color: Colors.black,
-          //   ),
-          //   onPressed: () => controller.onFavoritePressed(),
-          // ),
+          onPressed: () => controller.onCartPressed(),
+        ),
+        // TODO: Sistema de favoritos
+        // IconButton(
+        //   icon: Icon(
+        //     Icons.favorite_border,
+        //     color: Colors.black,
+        //   ),
+        //   onPressed: () => controller.onFavoritePressed(),
+        // ),
+      ],
+    );
+  }
+
+  _body() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 24.0),
+      child: ListView(
+        children: [
+          ImageSection(),
+          SizedBox(
+            height: 25,
+          ),
+          TitleAndPriceSection(),
+          SizedBox(
+            height: 16,
+          ),
+          DescriptionSection(controller.product.description),
+          SizedBox(
+            height: 17,
+          ),
+          MessageSection(),
         ],
       ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 24.0),
-        child: ListView(
+    );
+  }
+
+  _bottomNavBar() {
+    return Container(
+      height: 60,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 8.0),
+        child: Row(
           children: [
-            ImageSection(),
-            SizedBox(
-              height: 25,
+            Expanded(
+              child: Obx(() => AmountSelector(
+                    onAdd: controller.onAmountAddPressed,
+                    onRemove: controller.onAmountRemovePressed,
+                    amount: controller.amount,
+                  )),
             ),
-            TitleAndPriceSection(),
-            SizedBox(
-              height: 16,
-            ),
-            DescriptionSection(controller.product.description),
-            SizedBox(
-              height: 17,
-            ),
-            MessageSection(),
+            Expanded(
+              child: CustomBlueButton(
+                function: controller.onAddPressed,
+                text: "Adicionar",
+              ),
+            )
           ],
         ),
       ),
